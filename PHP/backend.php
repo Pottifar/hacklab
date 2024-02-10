b69b1f71d6cc364dea0b62f295aa9eb6191f1734
jostein <jostein.l@harrang.net>
2024-02-10 03:10:55 +0100

    Updated JS

--- PHP/backend.php
+++ PHP/backend.php
@@ -8,27 +8,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form data
-        $fname = $_POST['fname'];
-        $lname = $_POST['lname'];
+        $fname = $_POST['username'];
+        $lname = $_POST['password'];
       
         // Do something with the form data
         echo "Hello, " . htmlspecialchars($fname) . " " . htmlspecialchars($lname) . "!";
     }
 } 
-
-// Check if the request method is POST
-elseif ($_SERVER['REQUEST_METHOD'] == 'GET') {
-
-    if (isset($_GET['username'])) {
-        echo json_encode($_GET['username']);
-    } else {
-        echo json_encode("No username parameter provided");
-    }
-
-} else {
-    // If the request method is not GET or POST, return an error message
-    http_response_code(405);
-    echo json_encode(array("message" => "This method is not allowed."));
-}
-
 ?>
