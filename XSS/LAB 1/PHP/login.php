8c64da8dc6f6e555cb8259140350007a372bdcf5
jostein <jostein.l@harrang.net>
2024-02-10 14:43:26 +0100

    Fixed login

--- XSS/LAB 1/PHP/login.php
+++ XSS/LAB 1/PHP/login.php
@@ -4,6 +4,15 @@ header('Content-type: application/json');
 
 // Check if the request method is POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Connect to database
+    $servername = "192.168.140.130";
+    $username = "web-server";
+    $password = "web-server123";
+    $dbname = "Hacklab";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
     // Get the form data
     $username = $_POST['username'];
     $password = $_POST['password'];
@@ -14,15 +23,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         "password" => $password,
     );
 
-    // Convert the array to a JSON string
-    $json = json_encode($data);
-
-    // Save the JSON string to a session variable
-    session_start();
-    $_SESSION['data'] = $json;
-
-    // Redirect to another HTML page
-    header("Location: http://192.168.140.130/hacklab/XSS/LAB 1/PHP/userIsLoggedIn.php");
-    exit();
+    checkCredentials($conn, $username, $password);
 }
+
+function checkCredentials($conn, $username, $password) {
+   
+    // Prepare a SELECT statement to get the user's password
+    $stmt = $conn->prepare("SELECT id FROM Users WHERE username = ? AND password = ?");
+    $stmt->bind_param("ss", $username, $password);
+
+    // Execute the statement
+    $stmt->execute();
+
+    // Bind the result
+    $userID = "";
+    $stmt->bind_result($userID);
+
+    if($stmt->fetch()){
+        // Save the JSON string to a session variable
+        session_start();
+        $_SESSION['data'] = $userID;
+
+        // Redirect to another HTML page
+        header("Location: http://192.168.140.130/hacklab/XSS/LAB 1/PHP/userIsLoggedIn.php");
+        exit();
+    } else {
+        echo "Your username or password is incorrect.";
+    }
+}
+
 ?>
