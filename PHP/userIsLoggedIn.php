b3410c8a9a4363d5fc9ece1ba70b7ca34431c24f
jostein <jostein.l@harrang.net>
2024-02-10 03:28:24 +0100

    Updated JS

--- PHP/userIsLoggedIn.php
+++ PHP/userIsLoggedIn.php
@@ -5,18 +5,7 @@ session_start();
 
 // Check if the request method is GET
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    // Check if data is set in the session
-    if (isset($_SESSION['data'])) {
-        // Set the content type to application/json
-        header('Content-Type: application/json');
-
-        // Echo the data
-        echo $_SESSION['data'];
-    } else {
-        // If no data is set in the session, return an error message
-        http_response_code(404);
-        echo json_encode(array("message" => "User is not logged in."));
-    }
+    echo json_encode($_GET['username']);
 } else {
     // If the request method is not GET, return an error message
     http_response_code(405);
