4db7195e47ca821731023033681c1337d9e9b16c
jostein <jostein.l@harrang.net>
2024-02-10 02:02:08 +0100

    Added PHP endpoint

--- backend.php
+++ backend.php
@@ -1,5 +1,21 @@
-function loginSubmit(username, password) {
-    echo("Welcome " + username);
+<?php
+
+header('Content-type: application/json');
+
+// Check if the request method is GET
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+
+    // Your data here. This could also be a call to a database.
+    $data = array(
+        "message" => "Hello, this is your PHP endpoint!"
+    );
+
+    // Returns a JSON response
+    echo json_encode($data);
+} else {
+    // If the request method is not GET, return an error message
+    http_response_code(405);
+    echo json_encode(array("message" => "This method is not allowed."));
 }
 
-<script>alert(1)</script>
+?>
--- login.js
+++ login.js
@@ -4,3 +4,17 @@ function login() {
     console.log("Username is: " + username + " and password is " + password);
     document.cookie = "sessionid=testid"
 }
+
+function sendRequest(){
+    fetch('http://192.168.140.130//backend.php', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+    },
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch((error) => {
+    console.error('Error:', error);
+    });
+}
