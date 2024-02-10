bb05b3910504025f2711073cf4b07b8f936946b0
jostein <jostein.l@harrang.net>
2024-02-10 19:10:04 +0100

    Fixed login

--- XSS/LAB 1/PHP/application.php
+++ XSS/LAB 1/PHP/application.php
@@ -9,6 +9,45 @@ if (!isset($_SESSION['username'])) {
     exit();
 }
 
+function searchForItem($item) {
+    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+        // Connect to database
+        $servername = "localhost";
+        $username = "web-server";
+        $password = "web-server123";
+        $dbname = "Hacklab";
+        
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+    }
+    // Prepare a SELECT statement to get the user's hashed password
+    $stmt = $conn->prepare("SELECT itemName FROM Items WHERE itemName = ?");
+    $stmt->bind_param("s", $item);
+
+    // Execute the statement
+    $stmt->execute();
+
+    // Bind the result
+    $itemFound = null;
+    $stmt->bind_result($itemFound);
+    
+    // Fetch the result
+    $stmt->fetch();
+
+    if ($itemFound != "") {
+        // if the item is found, return it
+        return true;
+    } else {
+        // if the item is not found, return null or an appropriate message
+        return false; // or return "Item not found.";
+    }
+}
+
 // Check if the request method is GET
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     
@@ -24,9 +63,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         <title>User Page</title>
     </head>
     <body>
-        <h1>Welcome, $username!</h1>
-    </body>
-    </html>
+        <h1>Logged in as $username!</h1>
+        <form action='http://192.168.140.130/hacklab/XSS/LAB 1/PHP/searchForItem.php' method='get'>
+            <input type='text' name='query' placeholder='Search'>
+            <input type='submit' value='Search'>
+        </form>
+    
     ";
+    if (isset($_GET['item'])) {
+        $item = $_GET['item'];
+        echo "Results for $item:";
+        if(searchForItem($item)){
+            echo "$item was found!";
+        } else {
+            echo "$item was not found.";
+        }
+    } else {
+        echo "
+
+            </body>
+        </html>";
+    }
 }
 ?>
