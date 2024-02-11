d0d7388e97fb37cdfe774da240a476854b13bb94
jostein <jostein.l@harrang.net>
2024-02-11 02:25:48 +0100

    Fixed login

--- XSS/LAB 2/PHP/application.php
+++ XSS/LAB 2/PHP/application.php
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     <head>
         <meta charset='UTF-8'>
         <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-        <link rel='stylesheet' href='../CSS/style.css'>
+        <link rel='stylesheet' href='http://192.168.140.130/hacklab/XSS/LAB 2/CSS/style.css'>
         <title>User Page</title>
     </head>
     <body>
