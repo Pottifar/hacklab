bb0e068c2c912d59ccff06fbac65af9a3f01a39c
jostein <jostein.l@harrang.net>
2024-02-11 02:31:42 +0100

    Fixed login

--- XSS/LAB 2/PHP/application.php
+++ XSS/LAB 2/PHP/application.php
@@ -39,21 +39,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     </html>";
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Connect to database
-    $servername = "localhost";
-    $username = "web-server";
-    $password = "web-server123";
-    $dbname = "Hacklab";
-    
-    $title = $_POST['title'];
-    $text = $_POST['text'];
-
-    $stmt = $conn->prepare("INSERT INTO Posts (Title, Text, Votes) VALUES (?, ?, 0)");
-    $stmt->bind_param("ss", $title, $text);
-    $stmt->execute();
-}
-
 function displayPosts() {
 
     // Connect to database
--- XSS/LAB 2/PHP/createPost.php
+++ XSS/LAB 2/PHP/createPost.php
@@ -1,5 +1,22 @@
 <?php
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Connect to database
+    $servername = "localhost";
+    $username = "web-server";
+    $password = "web-server123";
+    $dbname = "Hacklab";
+    
+    $title = $_POST['title'];
+    $text = $_POST['text'];
 
+    $stmt = $conn->prepare("INSERT INTO Posts (Title, Text, Votes) VALUES (?, ?, 0)");
+    $stmt->bind_param("ss", $title, $text);
+    $stmt->execute();
+
+    // Redirect to another HTML page
+    header("Location: http://192.168.140.130/hacklab/XSS/LAB 2/PHP/application.php");
+    exit();
+}
 
 ?>
