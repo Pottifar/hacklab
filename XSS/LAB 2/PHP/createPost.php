c95918e0eaa665899f876b800121e611b2bc7b3f
jostein <jostein.l@harrang.net>
2024-02-11 02:35:43 +0100

    Fixed login

--- XSS/LAB 2/PHP/createPost.php
+++ XSS/LAB 2/PHP/createPost.php
@@ -1,24 +1,27 @@
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
     // Connect to database
     $servername = "localhost";
     $username = "web-server";
     $password = "web-server123";
     $dbname = "Hacklab";
 
-    echo "YUP";
-    
     $title = $_POST['title'];
     $text = $_POST['text'];
 
     $stmt = $conn->prepare("INSERT INTO Posts (Title, Text, Votes) VALUES (?, ?, 0)");
     $stmt->bind_param("ss", $title, $text);
-    $stmt->execute();
 
-    // Redirect to another HTML page
-    header("Location: http://192.168.140.130/hacklab/XSS/LAB 2/PHP/application.php");
-    exit();
+    if ($stmt->execute()) {
+        // If the statement executed successfully, redirect to application.php
+        header("Location: http://192.168.140.130/hacklab/XSS/LAB 2/PHP/application.php");
+        exit();
+    } else {
+        // Handle the error...
+        echo "Error: " . $stmt->error;
+    }
 }
 
 ?>
