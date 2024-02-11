cc2696d0c0553bbff6c35babbc5e02621166de89
jostein <jostein.l@harrang.net>
2024-02-11 02:14:09 +0100

    Fixed login

--- XSS/LAB 2/PHP/login.php
+++ XSS/LAB 2/PHP/login.php
@@ -1,28 +1,59 @@
 <?php
 
-header('Content-type: application/json');
-
 // Check if the request method is POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Connect to database
+    $servername = "localhost";
+    $username = "web-server";
+    $password = "web-server123";
+    $dbname = "Hacklab";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
     // Get the form data
     $username = $_POST['username'];
     $password = $_POST['password'];
-    
-    /// Create an associative array
-    $data = array(
-        "username" => $username,
-        "password" => $password,
-    );
 
-    // Convert the array to a JSON string
-    $json = json_encode($data);
-
-    // Save the JSON string to a session variable
-    session_start();
-    $_SESSION['data'] = $json;
-
-    // Redirect to another HTML page
-    header("Location: http://192.168.140.130/hacklab/XSS/LAB 2/PHP/application.php");
-    exit();
+    checkCredentials($conn, $username, $password);
 }
+
+function checkCredentials($conn, $username, $password) {
+    // Prepare a SELECT statement to get the user's hashed password
+    $stmt = $conn->prepare("SELECT hashedPassword FROM Users WHERE username = ?");
+    $stmt->bind_param("s", $username);
+
+    // Execute the statement
+    $stmt->execute();
+
+    // Bind the result
+    $hashedPassword = "";
+    $stmt->bind_result($hashedPassword);
+
+    // If a user with the given username exists
+    if ($stmt->fetch()){
+        // Verify the password
+        if (password_verify($password, $hashedPassword)) {
+            // Save the JSON string to a session variable
+            session_start();
+            $_SESSION['username'] = $username;
+
+            // Redirect to another HTML page
+            header("Location: http://192.168.140.130/hacklab/XSS/LAB 2/PHP/application.php");
+            exit();
+        } else {
+            echo "Your password or username is incorrect.";
+        }
+    } else {
+        echo "Your password or username is incorrect.";
+    }
+}
+
+
+
 ?>
